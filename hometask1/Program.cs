a057491c0396bdc75df06d190198ccc798129da7
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-09 14:23:43 +0300

    код задания 1

---
+++ hometask1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход пятизначное 
+// число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int ReadInt(string massage)
+{
+    System.Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int GetRange(int number)
+{
+    int a = number / 10000;
+    int b = (number / 1000) % 10;
+    int c = (number / 100) % 10;
+    int d = (number / 10) % 10;
+    int e = number % 10;
+
+    if (a == e && b == d)
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+    return 0;
+}
+
+
+int value = ReadInt("Введите пятизначное число");
+int a = GetRange(value);
+Console.WriteLine(a);
