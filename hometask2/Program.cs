e8e3e3ce0ac15548bca0c13c78524744081c20c8
Tolege Sultan <sultantulegenov91@gmail.com>
2023-04-09 14:46:50 +0300

    код задания 2

---
+++ hometask2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход
+// координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int ReadInt(string massage)
+{
+    System.Console.Write($"{massage} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int x1 = ReadInt("Введите первую точку координата х");
+int y1 = ReadInt("Введите первую точку координата у");
+int z1 = ReadInt("Введите первую точку координата z");
+int x2 = ReadInt("Введите вторую точку координата х");
+int y2 = ReadInt("Введите вторую точку координата у");
+int z2 = ReadInt("Введите вторую точку координата z");
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine("Расстояние между точками" +distance);
